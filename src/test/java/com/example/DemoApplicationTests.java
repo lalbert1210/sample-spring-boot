54b14986f8e81d50d6d4dc25dc56cc1afc82b19c
lalbert1210 <lalbert1210@gmail.com>
2020-01-16 11:49:01 +0100

    ajout test 2

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.log4j.Logger;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +14,11 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+        @Test
+      public void test_add() {
+    logger.error("TEST 2");
+    assertTrue(true); 
+    }
 
 }
